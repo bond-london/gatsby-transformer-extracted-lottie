ef04b0469d770b22a1a25e283d3e9dba19709835
nrandell <nick@nickrandell.com>
2021-03-30 16:50:32 +0100

    Initial version

---
+++ gatsby-node.js
@@ -0,0 +1,55 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createSchemaCustomization = exports.createResolvers = void 0;
+const fs_1 = __importDefault(require("fs"));
+const mini_svg_data_uri_1 = __importDefault(require("mini-svg-data-uri"));
+const svgo_1 = require("svgo");
+const lottie_to_svg_1 = __importDefault(require("lottie-to-svg"));
+async function parseLottie(path) {
+    const animationJson = await fs_1.default.promises.readFile(path, "utf8");
+    const animationData = JSON.parse(animationJson);
+    const svg = await lottie_to_svg_1.default(animationData, {});
+    const { data } = await svgo_1.optimize(svg, { multipass: true });
+    const encoded = mini_svg_data_uri_1.default(data);
+    return { animationJson, content: data, encoded };
+}
+function createResolvers(args) {
+    const { createResolvers, reporter } = args;
+    reporter.verbose("Creating resolvers for svg");
+    const resolvers = {
+        File: {
+            lottie: {
+                type: "ExtractedLottie",
+                async resolve(parent) {
+                    const name = parent.absolutePath.toLowerCase();
+                    reporter.verbose(`Looking at ${name}`);
+                    if (name.endsWith(".json")) {
+                        return await parseLottie(parent.absolutePath);
+                    }
+                    else {
+                        return null;
+                    }
+                },
+            },
+        },
+    };
+    createResolvers(resolvers);
+}
+exports.createResolvers = createResolvers;
+function createSchemaCustomization(args) {
+    const { reporter, actions: { createTypes }, } = args;
+    reporter.verbose("Create Lottie customization");
+    const typeDefs = `
+  type ExtractedLottie implements Node {
+    animationJson: String!
+    content: String!
+    encoded: String!
+  }
+`;
+    createTypes(typeDefs);
+}
+exports.createSchemaCustomization = createSchemaCustomization;
+//# sourceMappingURL=gatsby-node.js.map
---
+++ src/gatsby-node.ts
@@ -0,0 +1,54 @@
+import { CreateResolversArgs, CreateSchemaCustomizationArgs } from "gatsby";
+import fs from "fs";
+import svgToTinyDataUri from "mini-svg-data-uri";
+import { optimize } from "svgo";
+import { FileSystemNode } from "gatsby-source-filesystem";
+import renderToSvg from "lottie-to-svg";
+
+async function parseLottie(path: string) {
+  const animationJson = await fs.promises.readFile(path, "utf8");
+  const animationData = JSON.parse(animationJson);
+  const svg = await renderToSvg(animationData, {});
+  const { data } = await optimize(svg, { multipass: true });
+  const encoded = svgToTinyDataUri(data);
+  return { animationJson, content: data, encoded };
+}
+
+export function createResolvers(args: CreateResolversArgs) {
+  const { createResolvers, reporter } = args;
+  reporter.verbose("Creating resolvers for svg");
+  const resolvers = {
+    File: {
+      lottie: {
+        type: "ExtractedLottie",
+        async resolve(parent: FileSystemNode) {
+          const name = parent.absolutePath.toLowerCase();
+          reporter.verbose(`Looking at ${name}`);
+          if (name.endsWith(".json")) {
+            return await parseLottie(parent.absolutePath);
+          } else {
+            return null;
+          }
+        },
+      },
+    },
+  };
+  createResolvers(resolvers);
+}
+
+export function createSchemaCustomization(args: CreateSchemaCustomizationArgs) {
+  const {
+    reporter,
+    actions: { createTypes },
+  } = args;
+  reporter.verbose("Create Lottie customization");
+  const typeDefs = `
+  type ExtractedLottie implements Node {
+    animationJson: String!
+    content: String!
+    encoded: String!
+  }
+`;
+
+  createTypes(typeDefs);
+}
